ca698f3b63b8343ea2a6d7845b3c153fe50d8e94
matthew <matthew@rfh-engineering.com>
2020-10-29 16:54:31 +0000

    Fix python tests

--- Python/test.py
+++ Python/test.py
@@ -1,11 +1,11 @@
 import unittest
-from equation import wash_in_equation, 
+from equation import wash_in_equation, area_under_wash_in_equation
 
 class TestEquation(unittest.TestCase):
     def test_washInEquation(self):
         self.assertEqual(wash_in_equation(5),1.0)
-        self.assertAlmostEqual(wash_in_equation(15), 2.703796849,  delta=0.01)
+        self.assertAlmostEqual(wash_in_equation(15), 2.703796849, delta=0.01)
 
-    def teast_areaUnderCurve(self):
-        self.assertEqual(area_under_wash_in_equation(5),1.0)
-        self.assertAlmostEqual(area_under_wash_in_equation(15), 19.554623001714354,  delta=0.4)
+    def test_areaUnderCurve(self):
+        self.assertEqual(area_under_wash_in_equation(0.0), 0.0)
+        self.assertAlmostEqual(area_under_wash_in_equation(15), 19.5544378120266, delta=0.4)
