a05083ca686678924f71bc4a11a91239577fddb3
rhiannon <yenschofield@gmail.com>
2020-10-29 14:36:16 +0000

    add second python test

--- Python/test.py
+++ Python/test.py
@@ -2,5 +2,8 @@ import unittest
 from equation import wash_in_equation
 
 class TestEquation(unittest.TestCase):
-    def test(self):
-        self.assertEqual(wash_in_equation(0),0)
+    def test_1(self):
+        self.assertEqual(wash_in_equation(5),1.0)
+
+    def test_2(self):
+        self.assertAlmostEqual(wash_in_equation(15), 2.703796849,  delta=0.02703796849)
